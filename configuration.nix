b5247b6b12d0daa8f83f8e4210784e4b6d3a971f
InCodeo <githublife@proton.me>
2024-12-30 15:54:46 +1100

    v1

--- configuration.nix
+++ configuration.nix
@@ -1,39 +1,81 @@
 { config, pkgs, ... }:
 
 {
-  # Import hardware configuration
   imports = [ ./hardware-configuration.nix ];
 
-  networking.networkmanager.enable = true;
-  networking.wireless.enable = true;
+  # Enable flakes for future use
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Basic boot configuration
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";  # Typically for VMs
+  };
+
+  # Basic networking
+  networking = {
+    hostName = "nixos";
+    useDHCP = true;  # Simpler networking setup
+    firewall = {
+      allowedTCPPorts = [ 22 ];
+      trustedInterfaces = [ "tailscale0" ];
+      # Required for Tailscale
+      checkReversePath = "loose";
+    };
+  };
+
+  # Time and Locale
   time.timeZone = "Australia/Sydney";
   i18n.defaultLocale = "en_AU.UTF-8";
 
-  users.users.nixtest = {
-    isNormalUser = true;
-    description = "nixtest";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
+  # Enable Hyper-V guest services
+  virtualisation.hypervGuest.enable = true;
 
+  # Basic system packages
+  nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
+    # Core utilities
     wget
+    vim
     git
-    tailscale
+    curl
+    htop
+    btop
+    unzip
   ];
 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = true;
-      PermitRootLogin = "yes";
-      X11Forwarding = false;
+  # Services
+  services = {
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = true;
+        PermitRootLogin = "yes";
+      };
+      ports = [ 22 ];
     };
-    ports = [ 22 ];
+
+    tailscale.enable = true;
   };
 
-  services.tailscale.enable = true;
+  # ZSH Configuration
+  programs.zsh = {
+    enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "docker" "sudo" "kubectl" ];
+      theme = "robbyrussell";
+    };
+    shellInit = ''
+      ${builtins.replaceStrings ["\r\n"] ["\n"] (builtins.readFile ./functions.zsh)}
+      ${builtins.replaceStrings ["\r\n"] ["\n"] (builtins.readFile ./client-functions.zsh)}
+    '';
+  };
 
+  # Sets ZSH as default shell
+  users.defaultUserShell = pkgs.zsh;
+  users.users.root.shell = pkgs.zsh;
+
+  # This value determines the NixOS release with which your system is to be compatible
   system.stateVersion = "24.11";
 }
